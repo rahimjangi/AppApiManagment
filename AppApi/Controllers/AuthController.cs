bf9f0763d739baef7e185c6880a0ef2eab5e0475
pc <rahimjangi1982@gmail.com>
2023-03-14 21:46:19 +0100

    JWT added!

--- AppApi/Controllers/AuthController.cs
+++ AppApi/Controllers/AuthController.cs
@@ -4,7 +4,10 @@ using AppApi.Models;
 using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Tokens;
 using System.Data;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -40,16 +43,9 @@ public class AuthController : ControllerBase
                 {
                     rng.GetNonZeroBytes(passwordSalt);
                 }
-                string passwordSaltPlusString = _config.GetSection("AppSettings:PasswordKey").Value
-                    + Convert.ToBase64String(passwordSalt);
 
-                byte[] passwordHash = KeyDerivation.Pbkdf2(
-                    password: registrationDto.Password,
-                    salt: Encoding.ASCII.GetBytes(passwordSaltPlusString),
-                    prf: KeyDerivationPrf.HMACSHA256,
-                    iterationCount: 100000,
-                    numBytesRequested: 256
-                    );
+                byte[] passwordHash = GetPasswordHash(passwordSalt,registrationDto.Email);
+
                 string sqlAddAuth = @"INSERT INTO scott.Auth (
                                              [Email]
                                             ,[PasswordHash]
@@ -74,7 +70,28 @@ public class AuthController : ControllerBase
 
                 if(_dapper.ExecuteSqlWithParameters(sqlAddAuth, sqlParameters))
                 {
-                    return Ok();
+                    string sqlInsertUser = @"INSERT INTO SCOTT.USERS (
+                                            FirstName, 
+                                            LastName,
+                                            Email,
+                                            Gender,
+                                            Active
+                                        ) VALUES ('"
+                                            +registrationDto.FirstName+"', '" 
+                                            +registrationDto.LastName + "', '"
+                                            + registrationDto.Email + "', '"
+                                            + registrationDto.Gender + "', '"
+                                            + 1+"')";
+
+                    if(_dapper.ExecuteSql(sqlInsertUser, null))
+                    {
+                        return Ok();
+                    }
+                    else
+                    {
+                        return BadRequest("Failed to register the user!");
+                    }
+
                 }
                 else
                 {
@@ -94,6 +111,66 @@ public class AuthController : ControllerBase
     [HttpPost("[action]")]
     public IActionResult Login(UserForLoginDto loginDto)
     {
-        return Ok();
+        string sqlForHashAndSalt= @"select
+                                        [PasswordHash],
+                                        [PasswordSalt]
+                                   from scott.Auth
+                                         where Auth.Email ='"+loginDto.Email+ "'";
+
+        var userForLoginConfirmationDto=_dapper.LoadDataSingle<UserForLoginConfirmationDto>(sqlForHashAndSalt,null);
+        if (userForLoginConfirmationDto == null) { return BadRequest("User Does Not Exist") ; }
+
+        byte[] passwordHash = GetPasswordHash(userForLoginConfirmationDto.PasswordSalt, loginDto.Email);
+        for (int i = 0; i < passwordHash.Length; i++)
+        {
+            if (passwordHash[i]!= userForLoginConfirmationDto.PasswordHash[i])
+            {
+                return BadRequest("Email or password is not correct!");
+            }
+        }
+        int userId = _dapper.LoadDataSingle<int>("  select [UserId] from scott.USERS where Email='"+ loginDto.Email+ "'", null);
+        return Ok(new Dictionary<string, string>()
+        {
+            {"token",CreateToken(userId) }
+        });
+    }
+
+    private byte[] GetPasswordHash(byte[] passwordSalt,string password)
+    {
+        string passwordSaltPlusString = _config.GetSection("AppSettings:PasswordKey").Value
+            + Convert.ToBase64String(passwordSalt);
+
+        byte[] passwordHash = KeyDerivation.Pbkdf2(
+            password: password,
+            salt: Encoding.ASCII.GetBytes(passwordSaltPlusString),
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 100000,
+            numBytesRequested: 256
+            );
+        return passwordHash;
+    }
+
+    private string CreateToken(int userId)
+    {
+        string tokenKeyString = _config.GetSection("AppSettings:TokenKey").Value;
+        Claim[] claims = new Claim[]
+        {
+            new Claim("userId",userId.ToString())
+        };
+        SymmetricSecurityKey tokenKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKeyString));
+
+        SigningCredentials signingCredentials = new SigningCredentials(tokenKey,SecurityAlgorithms.HmacSha256Signature);
+
+        SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor()
+        {
+            Subject = new ClaimsIdentity(claims),
+            SigningCredentials = signingCredentials,
+            Expires= DateTime.Now.AddDays(30)
+        };
+
+        JwtSecurityTokenHandler tokenHandler= new JwtSecurityTokenHandler();
+        SecurityToken securityToken = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(securityToken);
     }
 }
--- AppApi/Dto/UserForRegistrationDto.cs
+++ AppApi/Dto/UserForRegistrationDto.cs
@@ -3,6 +3,10 @@
 public partial class UserForRegistrationDto
 {
     public string Email { get; set; } = string.Empty;
-    public string Password { get; set; }=string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Gender { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
     public string PasswordConfirm { get; set; } = string.Empty;
+    public bool Active { get; set; }
 }
